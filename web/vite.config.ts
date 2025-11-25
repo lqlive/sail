aa6582cd24aa7080d556b139e87ef60e9e9d6d00
lqlive <zze@live.com>
2025-11-25 11:18:42 +0800

    Add Docker support and update API routes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api/runtime': {
+      '/runtime': {
         target: 'http://localhost:80',
         changeOrigin: true,
         secure: false,
